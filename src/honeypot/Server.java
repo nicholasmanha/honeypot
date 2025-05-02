6bc4eeba3821ac0f6617c30c2db8cc360297832a
Nicholas Manha <nickrmanha@gmail.com>
2025-05-01 17:44:24 -0700

    added protocol messages

--- src/honeypot/Server.java
+++ src/honeypot/Server.java
@@ -2,6 +2,7 @@ package honeypot;
 
 import java.io.*;
 import java.net.*;
+import java.util.HexFormat;
 import java.util.logging.*;
 
 class Server {
@@ -17,11 +18,35 @@ class Server {
 		switch (args[0]) {
 		case "SSH":
 			port = 22;
-			message = "ssh";
+			message = "SSH-2.0-OpenSSH_9.9p1 Debian-3";
 			break;
 		case "HTTP":
 			port = 80;
-			message = "HTTP/1.1 200 OK\n";
+			message = "HTTP/1.1 200 OK\r\n"
+					+ "Server: nginx/1.18.0 (Ubuntu)\r\n"
+					+ "Date: Thu, 01 May 2025 21:23:17 GMT\r\n"
+					+ "Content-Type: text/html\r\n"
+					+ "Content-Length: 5124\r\n"
+					+ "Last-Modified: Wed, 22 Mar 2023 14:54:48 GMT\r\n"
+					+ "Connection: keep-alive\r\n"
+					+ "ETag: \"641b16b8-1404\"\r\n"
+					+ "Referrer-Policy: strict-origin-when-cross-origin\r\n"
+					+ "X-Content-Type-Options: nosniff\r\n"
+					+ "Feature-Policy: accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'\r\n"
+					+ "Content-Security-Policy: default-src 'self'; script-src cdnjs.cloudflare.com 'self'; style-src cdnjs.cloudflare.com 'self' fonts.googleapis.com 'unsafe-inline'; font-src fonts.googleapis.com fonts.gstatic.com cdnjs.cloudflare.com; frame-ancestors 'none'; report-uri https://scotthelme.report-uri.com/r/d/csp/enforce\r\n"
+					+ "Accept-Ranges: bytes\r\n" + "\r\n" + "<h1>Welcome to Honeypot</h1>";
+			break;
+		case "telnet":
+			port = 23;
+			message = "fffb03fffb01fffd18fffd1ffffd24fffd27fffd000d0a";
+			break;
+		case "ftp":
+			port = 21;
+			message = "220 ftp.scene.org FTP server (SceneOrgFTPD-2.4.4) ready.\r\n";
+			break;
+		case "smtp":
+			port = 25;
+			message = "220 zim.gshapiro.net ESMTP Sendmail 8.18.1.10/8.18.1.10; Fri, 2 May 2025 00:33:23 GMT";
 			break;
 		default:
 			System.err.println("ERROR: invalid service given");
@@ -65,7 +90,7 @@ class Server {
 			}
 		}
 	}
-
+	
 	// ClientHandler class
 	private class ClientHandler implements Runnable {
 		Logger logger = Logger.getLogger(Server.class.getName());
@@ -107,7 +132,18 @@ class Server {
 				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
 				// simulate the given service (from args)
-				out.println(message);
+				if(port != 23) {
+					out.println(message);
+				}
+				else {
+					OutputStream outputStream = clientSocket.getOutputStream();
+					
+					// convert string of hex into array of bytes
+					byte[] telnetResponse = HexFormat.of().parseHex(message);
+					
+					outputStream.write(telnetResponse);
+				}
+				
 				logger.info("Sent " + message + " to " + inet.getHostAddress());
 
 				String line;
@@ -117,7 +153,9 @@ class Server {
 					// writing the received message from
 					// client
 					System.out.printf(" Sent from the client: %s\n", line);
+					out.println(message);
 				}
+				
 			} catch (IOException e) {
 				logger.severe("Connection Error: " + e + " from " + inet.getHostAddress());
 				e.printStackTrace();
